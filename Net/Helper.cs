80bec0442bded947f65bf2c6b669b81569929c4b
workwhileweb <workwhileweb@gmail.com>
2020-03-13 15:31:03 +0700

    first commit : thêm thư viện ssh2socks

---
+++ Net/Helper.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+// ReSharper disable IdentifierTypo
+// ReSharper disable UnusedMember.Global
+
+namespace TuyetVoi.Net
+{
+    public class Helper
+    {
+        public static int GetFreeTcpPortV3()
+        {
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Bind(new IPEndPoint(IPAddress.Loopback, port: 0));
+                return ((IPEndPoint)socket.LocalEndPoint).Port;
+            }
+        }
+
+        public static int GetFreeTcpPortV2(int startingPort)
+        {
+            var properties = IPGlobalProperties.GetIPGlobalProperties();
+
+            //getting active connections
+            var tcpConnectionPorts = properties.GetActiveTcpConnections()
+                .Where(n => n.LocalEndPoint.Port >= startingPort)
+                .Select(n => n.LocalEndPoint.Port);
+
+            //getting active tcp listeners - WCF service listening in tcp
+            var tcpListenerPorts = properties.GetActiveTcpListeners()
+                .Where(n => n.Port >= startingPort)
+                .Select(n => n.Port);
+
+            //getting active udp listeners
+            var udpListenerPorts = properties.GetActiveUdpListeners()
+                .Where(n => n.Port >= startingPort)
+                .Select(n => n.Port);
+
+            var port = Enumerable
+                .Range(startingPort, ushort.MaxValue)
+                .Where(i => !tcpConnectionPorts.Contains(i))
+                .Where(i => !tcpListenerPorts.Contains(i))
+                .FirstOrDefault(i => !udpListenerPorts.Contains(i));
+
+            return port;
+        }
+
+        public static int GetFreeTcpPort()
+        {
+            var l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            var port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
+        }
+    }
+}
---
+++ Net/Ssh2Socks.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using Renci.SshNet;
+
+namespace TuyetVoi.Net
+{
+    public class Ssh2Socks : IDisposable
+    {
+        public ForwardedPortDynamic ForwardedPort;
+
+        public SshClient Ssh;
+
+        public Ssh2Socks(IPAddress ip, string user, string pass)
+            : this(ip, 22, user, pass)
+        {
+        }
+
+        public Ssh2Socks(IPAddress ip, uint port, string user, string pass)
+            : this(ip, port, 0, user, pass)
+        {
+        }
+
+        public Ssh2Socks(IPAddress ip, uint port, uint socksPort, string user, string pass)
+            : this(ip, port, socksPort, user, pass, null)
+        {
+        }
+
+        public Ssh2Socks(string ip, uint port, uint socksPort, string user, string pass, string keyFile)
+            : this(IPAddress.Parse(ip), port, socksPort, user, pass, keyFile)
+        {
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        /// <param name="socksPort"></param>
+        /// <param name="user"></param>
+        /// <param name="pass"></param>
+        /// <param name="keyFile"></param>
+        public Ssh2Socks(IPAddress ip, uint port, uint socksPort, string user, string pass, string keyFile)
+        {
+            Ip = ip;
+            Port = port <= 0 ? 22 : port;
+            User = user;
+            Pass = pass;
+            KeyFile = keyFile;
+
+            Ssh = KeyFile == null
+                ? new SshClient(Ip.ToString(), (int)Port, User, Pass)
+                : new SshClient(Ip.ToString(), (int)Port, User, new PrivateKeyFile(KeyFile));
+            Ssh.Connect();
+            
+            SocksPort = socksPort;
+            if (SocksPort <= 0) SocksPort = (uint) Helper.GetFreeTcpPort();
+
+            ForwardedPort = new ForwardedPortDynamic("127.0.0.1", SocksPort);
+            Ssh.AddForwardedPort(ForwardedPort);
+            ForwardedPort.Start();
+        }
+
+        public IPAddress Ip { get; }
+        public uint Port { get; }
+        public uint SocksPort { get; }
+        public string User { get; }
+        public string Pass { get; }
+        public string KeyFile { get; }
+
+        public void Dispose()
+        {
+            ForwardedPort.Stop();
+            ForwardedPort.Dispose();
+            Ssh.Disconnect();
+            Ssh?.Dispose();
+        }
+    }
+}
